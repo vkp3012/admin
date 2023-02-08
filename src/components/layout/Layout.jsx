e75b0a9e5d4749646bd3feaec90957256074d305
vkp3012 <vivekpatel301295@gmail.com>
2023-02-08 12:58:39 +0530

    create a routes

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import Sidebar from '../sidebar/Sidebar';
+import Routes from '../Routes';
 
 const Layout = () => {
     return (
-        <div>
-            <h1>Hay Layout!</h1>       
-        </div>
+        <BrowserRouter>
+            <Route render = {(props)=>(
+                <div className='layout'>
+                    <Sidebar {...props}/>
+                    <div className="layout__content">
+                        <div className="layout__content-main">
+                            <Routes />
+                        </div>
+                    </div>
+                </div>
+            )}/>
+        </BrowserRouter>
     )
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard Open
+        </div>
+    )
+}
+
+export default Dashboard
