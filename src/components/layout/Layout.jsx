ba253ec835e22de0d499a483b0e1828a80f7e77f
vkp3012 <vivekpatel301295@gmail.com>
2023-02-09 10:24:22 +0530

    create a product details board

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom';
 import Sidebar from '../sidebar/Sidebar';
 import Routes from '../Routes';
 import './layout.css';
 import TopNav from '../topnav/TopNav';
+// import { useDispatch, useSelector } from 'react-redux';
 
 const Layout = () => {
+    // const themeReducer = useSelector(state => state.ThemeReducer)
+    // const dispatch = useDispatch();
+    // useEffect(()=>{
+    //     const themeClass = localStorage.getItem('themeMode',"theme-mode-light")
+    //     const colorClass = localStorage.getItem('colorMode',"theme-mode-light")
+    // })
     return (
         <BrowserRouter>
             <Route render = {(props)=>(
