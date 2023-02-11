d55e634bb5ba6025fef7e372779667428a9e005c
VIVEK KUMAR PATEL <76051796+vkp3012@users.noreply.github.com>
2023-02-11 16:24:18 +0530

    Update Dashboard.jsx
    all done

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,44 +1,44 @@
 import React from 'react'
-// import { Link } from "react-router-dom";
-// import Chart from "react-apexcharts";
-// import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Chart from "react-apexcharts";
+import { useSelector } from "react-redux";
 import StatusCard from "../components/status-card/StatusCard"
-// import Badge from "../components/badge/Badge";
+import Badge from "../components/badge/Badge";
 import statusCards from '../assets/JsonData/status-card-data.json';
-// const chartOptions = {
-//     series : [{
-//         name : 'Online Customers',
-//         data : [40,70,20,90,36,80,30,91,60]
-//     },{
-//         name: 'Store Customers',
-//         data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
-//     }],
-//     options: {
-//         color: ['#6ab04c', '#2980b9'],
-//         chart: {
-//             background: 'transparent'
-//         },
-//         dataLabels: {
-//             enabled: false
-//         },
-//         stroke: {
-//             curve: 'smooth'
-//         },
-//         xaxis: {
-//             categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
-//         },
-//         legend: {
-//             position: 'top'
-//         },
-//         grid: {
-//             show: false
-//         }
-//     }
-// }
+const chartOptions = {
+    series : [{
+        name : 'Online Customers',
+        data : [40,70,20,90,36,80,30,91,60,10]
+    },{
+        name: 'Store Customers',
+        data: [40, 30, 70, 80, 40, 16, 40, 20, 51, 10]
+    }],
+    options: {
+        color: ['#6ab04c', '#2980b9'],
+        chart: {
+            background: 'transparent'
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth'
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+        },
+        legend: {
+            position: 'top'
+        },
+        grid: {
+            show: false
+        }
+    }
+}
 
 
 const Dashboard = () => {
-    // const themeReducer = useSelector(state => state.themeReducer.node);
+    const themeReducer = useSelector(state => state.themeReducer.node);
     return (
         <div>
             <h2 className='page-header'>Dashboard</h2>
@@ -58,7 +58,7 @@ const Dashboard = () => {
                         }
                     </div>
                 </div>
-                {/* <div className="col-6">
+                <div className="col-6">
                     <div className="card full-height">
                         <Chart 
                             options={themeReducer === 'theme-mode-dark' ? {
@@ -73,7 +73,7 @@ const Dashboard = () => {
                             height= '100%'
                         />
                     </div>
-                </div> */}
+                </div>
             </div>
         </div>
     )
